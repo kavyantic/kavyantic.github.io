407a6699dedf791e3b59c7f5adcc2aad06811877
sharmakavya281105 <kavyarahul9610@gmail.com>
2023-04-07 18:39:57 +0530

    new export with chages

--- docs/_next/static/QXAhPgSR0mNoAjFh5voGu/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-1955c6bb87032584.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
