03771d65f3d5740d62b6095d74835dd79f8bf289
sharmakavya281105 <kavyarahul9610@gmail.com>
2023-04-07 17:58:49 +0530

    docs folder added for github.io

---
+++ docs/_next/static/QXAhPgSR0mNoAjFh5voGu/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-1955c6bb87032584.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
